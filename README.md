a2310fbda33de200a63bfec14d37e233d998013e
loorlab-team <labloor@gmail.com>
2021-01-16 00:58:50 -0500

    p5js - Demo 1 - 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # visualoor
 A E S / T H 3 1 C
+
+# tutorial
+https://www.youtube.com/playlist?list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA
--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,25 @@
-function setup() {
-    createCanvas(400, 400);
+
+/* Demo 1 */
+/*function setup() {
+    createCanvas(1920, 600);
+    line(15, 25, 70, 90);
 }
 
 function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+}*/
+
+/* Demo 2 */
+function setup() {
+  drawingContext.shadowOffsetX = 5;
+  drawingContext.shadowOffsetY = -5;
+  drawingContext.shadowBlur = 10;
+  drawingContext.shadowColor = "black";
+  background(200);
+  ellipse(width/2, height/2, 50, 50);
 }
